89c1c7bd0cdf464af9643fb902967711d1dd8603
Arsh <arshdhiman458@gmail.com>
2024-08-07 09:04:55 +0530

    Update 5.check_Occurrences.cpp

--- 5.arrays/5.check_Occurrences.cpp
+++ 5.arrays/5.check_Occurrences.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
 	int a[30],i,count=0,n,x;
-	cout<<"enter no. of elements : ";
+	cout<<"enter no. of elements: ";
 	cin>>n;
 	cout<<"enter elements \n";
 	for(i=0;i<n;i++){
 		cin>>a[i];
 	}
-	cout<<" enter element to check its occurrence :";
+	cout<<" enter element to check its occurrence: ";
 	cin>>x;
 	for(i=0;i<n;i++){
 		if(a[i]==x){
